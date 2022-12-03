9de243145f3b13548e7971261195d54863ed0503
robertocannella <robertocannella@gmail.com>
2022-12-02 20:20:12 -0500

    Add Copy Overlay Animation

--- src/ColorBox.tsx
+++ src/ColorBox.tsx
@@ -6,23 +6,41 @@ type ColorBoxProps = {
     background: string,
     name: string
 }
+type ColorBoxState = {
+    isCopied: boolean
+}
 
-class ColorBox extends Component<ColorBoxProps,any> {
+class ColorBox extends Component<ColorBoxProps,ColorBoxState> {
     constructor(props:ColorBoxProps) {
         super(props);
-
+        this.state = {
+            isCopied: false
+        }
+        this.changeCopyState = this.changeCopyState.bind(this);
     }
-
+    changeCopyState(){
+        this.setState({isCopied:true},()=>{
+            setTimeout(()=>{
+                this.setState({isCopied:false},)
+            },1500)
+        })
+    }
     render() {
         const {name, background} = this.props;
+        const {isCopied} = this.state;
         return (
-            <CopyToClipboard text={background}>
+            <CopyToClipboard text={background} onCopy={this.changeCopyState}>
                 <div style={{ background }} className={"ColorBox"}>
+                    <div style={{ background }} className={`copy-overlay ${isCopied && 'show'}`}></div>
+                    <div className={`copy-msg ${isCopied && 'show'}`}>
+                        <h1>copied</h1>
+                        <p>{background}</p>
+                    </div>
                 <div className="copy-container">
                     <div className="box-content">
                         <span>{name}</span>
                     </div>
-                    <button className="copy-button">
+                    <button  className={"copy-button"}>
                         Copy
                     </button>
                 </div>
